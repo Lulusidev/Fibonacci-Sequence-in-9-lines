58d872d0c1f07a8c8915933f49ad2d2f509f334a
DevAlek <78972830+DevAlek@users.noreply.github.com>
2022-01-07 00:56:33 -0300

    Ops, its `-2`, not `+1` haha

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,5 +2,5 @@
 from functools import lru_cache
 @lru_cache
 def fibonacci(number: int) -> int:
-	return 1 if number <= 1 else (fibonacci(number - 1) + fibonacci(number + 1))
+	return 1 if number <= 1 else (fibonacci(number - 1) + fibonacci(number - 2))
 print(fibonacci(int(input('Tamanho da fibonacci sequence que você quer: '))))
